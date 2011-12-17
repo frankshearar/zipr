9b6f32207ff2ffbf5e4ad6a533f35593aca24f53
Frank Shearar <frank.shearar@gmail.com>
2011-12-17 23:53:45 +0000

    #17: stress test the trampoline.

---
+++ test/zipper_stresstest.rb
@@ -0,0 +1,33 @@
+require 'zipr/zipper'
+require 'zipr/test-trees'
+require 'rantly/property'
+require 'rspec'
+require 'zipr/rantly-extensions'
+
+module Zipr
+  describe "Zipper stress tests" do
+    it "should be able to process large structures" do
+      # The traversal uses a trampoline to navigate to the (great)^n-uncle
+      # of a rightmost node. This test throws a pathological tree at the
+      # traversal to demonstrate that the trampoline does, indeed, permit
+      # extremely deep recursion.
+      deepness = 10000
+
+      short_right = Leaf.new(2)
+      long_left = Leaf.new(1)
+      deepness.downto(1) { |i|
+        long_left = Node.new(i, [long_left])
+      }
+      tree = Node.new(0, [long_left, short_right])
+      t = PreOrderTraversal.new(tree.zipper)
+      arr = []
+      t.each { |node|
+        case node
+          when Node then arr << node.tag
+          when Leaf then;
+        end
+      }
+      arr.should == (0..deepness).to_a
+    end
+  end
+end
