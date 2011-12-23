9f3221adc455b74dd3e1b208a90e7b8ca024dfe1
Frank Shearar <frank.shearar@gmail.com>
2011-12-23 22:05:38 +0000

    Break out the test-trees tests into a separate file.

--- lib/zipr/rantly-extensions.rb
+++ lib/zipr/rantly-extensions.rb
@@ -1,7 +1,30 @@
 require 'rantly/property'
+require 'zipr/test-trees'
 
 class Rantly
   def any
     choose(integer, string, boolean)
+  end
+
+  def max_subtrees
+    10
+  end
+
+  def tree(n = self.size)
+    if (n <= 0) then
+      Zipr::EmptyTree.new
+    elsif (n == 1) then
+      Zipr::Leaf.value(any)
+    else
+      num_subtrees = integer(0..max_subtrees)
+      subtrees = []
+      sz = n
+      while (sz > 0) do
+        t = tree(integer(0..(n - 1)))
+        subtrees << t
+        sz -= t.size
+      end
+      Zipr::Node.new(any, subtrees)
+    end
   end
 end
